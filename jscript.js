212480cec6a7ae1d39e7699ef5ae4321ec1301e8
Matthew Johnson <johnson.2534@gmail.com>
2016-10-08 12:16:13 -0400

    Almost Done, mouseOver broke

--- jscript.js
+++ jscript.js
@@ -1,14 +1,36 @@
 $(document).ready(function() {
-  for(i = 1; i <= 16; i++) {
-    $('.gridContainer').append("<div class='grid'></div>");
+
+
+
+  function createGrid(size)  {
+
+    for(i = 1; i <= Math.pow(size,2); i++) {
+      $('.gridContainer').append("<div class='grid'></div>");
+    };
+    var gridDimension = 480/size;
+    $('div .grid').css('width', gridDimension);
+    $('div .grid').css('height', gridDimension);
   };
 
-  $('.grid').on('mouseenter', function()  {
-    var orgColor = $(this).css('background-color');
-    $(this).css('background-color', '#6313db');
-
-
-  
+  $('button').click(function()  {
+    var defaultDivs = document.getElementById('gridContainer');
+    while (defaultDivs.hasChildNodes()) {
+      defaultDivs.removeChild(defaultDivs.firstChild);
+    };
+    var userSize = +window.prompt("How many Squares per Row");
+    createGrid(userSize);
   });
+
+  createGrid(4);
+
+  $('.grid').on('mouseenter', function()  {
+  var orgColor = $(this).css('background-color');
+  $(this).css('background-color', '#6313db');
+
+});
+
+
+
+
 
 });
